c7be83db2184ab52f68d72223efbcbef17312a68
Irfan470 <amonium7@gmail.com>
2024-08-27 15:23:42 +0200

    inheritance

--- inheritance.js
+++ inheritance.js
@@ -1,4 +1,4 @@
-class appliance {
+class Appliance {
   constructor(brand, power) {
     this.brand = brand;
     this.power = power;
@@ -7,3 +7,27 @@ class appliance {
     return `${this.brand} consumes ${this.power} watts`;
   }
 }
+class Fridge extends Appliance {
+    constructor(brand, power, temperature) {
+      super(brand, power);
+      this.temperature = temperature;
+    }
+    description() {
+      return super.description() + ` and keeps a temperature of ${this.temperature} degrees`;
+    }
+}
+const myFridge = new Fridge('Samsung', 500, 4);
+console.log(myFridge.description()); // Samsung consumes 500 watts and keeps a temperature of 4 degrees
+
+class Oven extends Appliance {
+    constructor(brand, power, volume) {
+      super(brand, power);
+      this.volume = volume;
+    }
+    description() {
+        return super.description() + ` and capacity up to ${this.volume} liters`; 
+}
+}
+
+const myOven = new Oven('Whirlpool', 1500, 20);
+console.log(myOven.description()); // Whirlpool consumes 1500 watts and heats up to 200 degrees
