ae227cce4735059417461971686ba66f606bb314
Irfan470 <amonium7@gmail.com>
2024-09-27 10:53:26 +0200

    task 3.2

--- FET/3.2/task.js
+++ FET/3.2/task.js
@@ -1,5 +1,20 @@
-function action(){
+function action1(){
     $("div p").replaceWith(replacement);
 }
 function replacement(){
     return '<a href="https://www.youtube.com">' + $(this).text()+'</a>';}
+
+    function action2(){
+        $("a").hover(hoverIn, hoverOut);
+
+    }
+    function hoverIn(){
+        $(this).css("color", "red");
+    }
+    function hoverOut(){
+        $(this).css("color", "black");
+    }
+    function action3(){
+        $("p").fadeOut(1000);
+        $("button").slideUp(1000);
+    }
